4c1b1de35b02c6af4c0a3e856c1a89a95a63e284
Pieter Hoste <hoste.pieter@gmail.com>
2012-10-27 17:10:08 +0200

    Cleaned out the code. Now supports more directories to copy from and to, including the adminhtml directories and the code directories

--- MagentoUtils.py
+++ MagentoUtils.py
@@ -1,14 +1,25 @@
-import sublime, sublime_plugin
-import re, os, shutil
+import sublime_plugin
+import re
+import os
+import shutil
+
 
 class MagentoUtilsCopyToPackageCommand(sublime_plugin.WindowCommand):
-    def run(self, paths = []):
-        mage_path = MagentoUtilsCopyToPackageCommand.__validMagentoPaths(paths)
-        if mage_path is None:
-            return False
+    copy_paths = {
+                    'app/code/core/': 'app/code/local/',
+                    'app/code/community/': 'app/code/local/',
+                    'app/design/adminhtml/default/default/': 'app/design/adminhtml/default/!CUSTOM_PACKAGE!/',
+                    'app/design/frontend/base/': 'app/design/frontend/!CUSTOM_PACKAGE!/',
+                    'app/design/frontend/default/': 'app/design/frontend/!CUSTOM_PACKAGE!/',
+                    'skin/adminhtml/default/default/': 'skin/adminhtml/default/!CUSTOM_PACKAGE!/',
+                    'skin/frontend/base/': 'skin/frontend/!CUSTOM_PACKAGE!/',
+                    'skin/frontend/default/': 'skin/frontend/!CUSTOM_PACKAGE!/'
+                 }
 
+    def run(self, paths=[]):
         for path in paths:
-            to_path = MagentoUtilsCopyToPackageCommand.__getToPath(path, mage_path)
+            to_path = MagentoUtilsCopyToPackageCommand.__validateAndGetToPath(path)
+
             if to_path is not None and not os.path.exists(to_path):
                 if os.path.isdir(path):
                     shutil.copytree(path, to_path)
@@ -20,13 +31,9 @@ class MagentoUtilsCopyToPackageCommand(sublime_plugin.WindowCommand):
 
                 print "copied from: " + path + " to: " + to_path
 
-    def is_visible(self, paths = []):
-        mage_path = MagentoUtilsCopyToPackageCommand.__validMagentoPaths(paths)
-        if mage_path is None:
-            return False
-
+    def is_visible(self, paths=[]):
         for path in paths:
-            to_path = MagentoUtilsCopyToPackageCommand.__getToPath(path, mage_path)
+            to_path = MagentoUtilsCopyToPackageCommand.__validateAndGetToPath(path)
 
             if to_path is None or os.path.exists(to_path):
                 return False
@@ -34,81 +41,55 @@ class MagentoUtilsCopyToPackageCommand(sublime_plugin.WindowCommand):
         return True
 
     @staticmethod
-    def __getToPath(path, mage_path):
-        package = MagentoUtilsCopyToPackageCommand.__getMagentoPackageFromPath(path, mage_path)
-        theme = MagentoUtilsCopyToPackageCommand.__getMagentoThemeFromPath(path, mage_path)
-
-        if package is None or theme is None:
+    def __validateAndGetToPath(path):
+        result = MagentoUtilsCopyToPackageCommand.__validMagentoPaths(path)
+        if result is None:
             return None
 
-        custom_package = MagentoUtilsCopyToPackageCommand.__figureOutCustomPackage(path, mage_path)
-        custom_theme = 'default' # assume custom theme is always 'default'
+        from_path_rel = result.get('from')
+        to_path_rel = result.get('to')
 
-        if custom_package is None or custom_theme is None:
+        result = MagentoUtilsCopyToPackageCommand.__getBasePathAndRelPath(path, from_path_rel)
+        if result is None:
             return None
 
-        base_path = MagentoUtilsCopyToPackageCommand.__getBasePath(path, mage_path)
-        rel_from_path = MagentoUtilsCopyToPackageCommand.__getRelFromPath(path, mage_path, package, theme)
+        base_path = result.get('base')
+        path_rel = result.get('rel_path')
 
-        if base_path is None or rel_from_path is None:
-            return None
+        return MagentoUtilsCopyToPackageCommand.__getToPath(base_path, to_path_rel, path_rel)
 
-        to_path = os.path.join(base_path, mage_path, custom_package, custom_theme, rel_from_path)
+    @staticmethod
+    def __getToPath(base_path, to_path_rel, path_rel):
+        to_path = os.path.join(base_path, to_path_rel, path_rel)
+        if '!CUSTOM_PACKAGE!' in to_path:
+            custom_package = MagentoUtilsCopyToPackageCommand.__figureOutCustomPackage(base_path)
+            to_path = to_path.replace('!CUSTOM_PACKAGE!', custom_package)
+
         return to_path
 
     @staticmethod
-    def __validMagentoPaths(paths):
-        for path in paths:
-            if "app/design/frontend/" in path:
-                return "app/design/frontend/"
-            elif "skin/frontend/" in path:
-                return "skin/frontend/"
+    def __validMagentoPaths(path):
+        for from_path, to_path in MagentoUtilsCopyToPackageCommand.copy_paths.items():
+            if from_path in path:
+                return {'from': from_path, 'to': to_path}
 
         return None
 
     @staticmethod
-    def __getMagentoPackageFromPath(path, mage_path):
-        matches = re.search('%s([^/]+)/' % mage_path, path)
-        if matches is not None:
-            return matches.group(1)
+    def __figureOutCustomPackage(base_path):
+        base_path = os.path.join(base_path, 'app/design/frontend')
+        if os.path.isdir(base_path):
+            for f in os.listdir(base_path):
+                if os.path.isdir(os.path.join(base_path, f)):
+                    if f != 'base' and f != 'default':
+                        return f
 
         return None
 
     @staticmethod
-    def __getMagentoThemeFromPath(path, mage_path):
-        matches = re.search('%s[^/]+/([^/]+)' % mage_path, path)
+    def __getBasePathAndRelPath(path, from_path_rel):
+        matches = re.search('(.+)%s(.+)' % from_path_rel, path)
         if matches is not None:
-            return matches.group(1)
+            return {'base': matches.group(1), 'rel_path': matches.group(2)}
 
         return None
-
-    @staticmethod
-    def __figureOutCustomPackage(path, mage_path):
-        matches = re.search('(^.+%s).+' % mage_path, path)
-        if matches is not None:
-            packages_path = matches.group(1)
-            if os.path.isdir(packages_path):
-                for f in os.listdir(packages_path):
-                    if os.path.isdir(os.path.join(packages_path, f)):
-                        if f != "base" and f != "default":
-                            return f
-
-        return None
-
-    @staticmethod
-    def __getBasePath(path, mage_path):
-        matches = re.search('(.+)%s' % mage_path, path)
-        if matches is not None:
-            return matches.group(1)
-
-        return None
-
-    @staticmethod
-    def __getRelFromPath(path, mage_path, package, theme):
-        package_theme_path = os.path.join(mage_path, package, theme)
-        matches = re.search('.+%s/(.+)$' % package_theme_path, path)
-        if matches is not None:
-            return matches.group(1)
-
-        return None
-
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Currently only provides one functionality:
 
 * When right clicking on a file or directory inside the base or default packages from magento, it will show a menu item to copy that file or directory into your custom package.
+* Also does the same for code/core and code/community => code/local directories

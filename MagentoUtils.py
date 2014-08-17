2e6729aa402155ecf01a2f0697b2cfceb7c97a53
Pieter Hoste <hoste.pieter@gmail.com>
2014-08-17 23:41:26 +0200

    A long overdue update
    - Made it possible to define your custom packages in a sublime-project
    file.
    - Updated README with an example
    - Excluded ‘enterprise’ package when guessing custom package name
    (untested)
    - Now opening file after you copied it
    - Renamed some stuff
    - Fixes #1

--- MagentoUtils.py
+++ MagentoUtils.py
@@ -1,24 +1,30 @@
+# WARNING: this code is probably terrible (I'm a Python illiterate), but it seems to work, and that's what matters :)
+
+import sublime
 import sublime_plugin
 import re
 import os
 import shutil
 
 
-class MagentoUtilsCopyToPackageCommand(sublime_plugin.WindowCommand):
-    copy_paths = {
-                    'app/code/core/': 'app/code/local/',
-                    'app/code/community/': 'app/code/local/',
-                    'app/design/adminhtml/default/default/': 'app/design/adminhtml/default/!CUSTOM_PACKAGE!/',
-                    'app/design/frontend/base/': 'app/design/frontend/!CUSTOM_PACKAGE!/',
-                    'app/design/frontend/default/': 'app/design/frontend/!CUSTOM_PACKAGE!/',
-                    'skin/adminhtml/default/default/': 'skin/adminhtml/default/!CUSTOM_PACKAGE!/',
-                    'skin/frontend/base/': 'skin/frontend/!CUSTOM_PACKAGE!/',
-                    'skin/frontend/default/': 'skin/frontend/!CUSTOM_PACKAGE!/'
-                 }
+class MagentoUtilsCopyToOtherDirectoryCommand(sublime_plugin.WindowCommand):
+    default_copy_paths = {
+        'app/code/core/': 'app/code/local/',
+        'app/code/community/': 'app/code/local/',
+    }
+    default_copy_paths_with_guessing = {
+        'base/default': '!GUESS_CUSTOM_PACKAGE!/default',
+        'default/default': '!GUESS_CUSTOM_PACKAGE!/default',
+        'base/default': '!GUESS_CUSTOM_PACKAGE!/default',
+        'default/default': '!GUESS_CUSTOM_PACKAGE!/default'
+    }
+
+    def __init__(self, window):
+        sublime_plugin.WindowCommand.__init__(self, window)
 
     def run(self, paths=[]):
         for path in paths:
-            to_path = MagentoUtilsCopyToPackageCommand.__validateAndGetToPath(path)
+            to_path = MagentoUtilsCopyToOtherDirectoryCommand.__validateAndGetToPath(path)
 
             if to_path is not None and not os.path.exists(to_path):
                 if os.path.isdir(path):
@@ -28,12 +34,13 @@ class MagentoUtilsCopyToPackageCommand(sublime_plugin.WindowCommand):
                     if not os.path.exists(to_path_dir):
                         os.makedirs(to_path_dir)
                     shutil.copy(path, to_path)
+                    sublime.active_window().open_file(to_path)
 
-                print "copied from: " + path + " to: " + to_path
+                sublime.status_message("[MagentoUtils] Copied ...%s to ...%s" % (path[-50:], to_path[-50:]))
 
     def is_visible(self, paths=[]):
         for path in paths:
-            to_path = MagentoUtilsCopyToPackageCommand.__validateAndGetToPath(path)
+            to_path = MagentoUtilsCopyToOtherDirectoryCommand.__validateAndGetToPath(path)
 
             if to_path is None or os.path.exists(to_path):
                 return False
@@ -41,47 +48,66 @@ class MagentoUtilsCopyToPackageCommand(sublime_plugin.WindowCommand):
         return True
 
     @staticmethod
+    def __getCopyPaths():
+        if sublime.active_window() and sublime.active_window().active_view() and sublime.active_window().active_view().settings():  # sometimes this isn't working, I don't know why ...
+            project_specific_packages = sublime.active_window().active_view().settings().get('magento_utils_packages_to_copy_between', MagentoUtilsCopyToOtherDirectoryCommand.default_copy_paths_with_guessing)
+        else:
+            project_specific_packages = MagentoUtilsCopyToOtherDirectoryCommand.default_copy_paths_with_guessing
+
+        project_specific_packages_with_prefixes = {}
+
+        for from_path, to_path in project_specific_packages.items():
+            # trim slashes on both sides of paths, otherwise there could be trouble further down the line!
+            from_path = from_path.strip('/')
+            to_path = to_path.strip('/')
+
+            project_specific_packages_with_prefixes["app/design/frontend/%s/" % from_path] = "app/design/frontend/%s/" % to_path
+            project_specific_packages_with_prefixes["skin/frontend/%s/" % from_path] = "skin/frontend/%s/" % to_path
+
+        return dict(MagentoUtilsCopyToOtherDirectoryCommand.default_copy_paths.items() + project_specific_packages_with_prefixes.items())
+
+    @staticmethod
     def __validateAndGetToPath(path):
-        result = MagentoUtilsCopyToPackageCommand.__validMagentoPaths(path)
+        result = MagentoUtilsCopyToOtherDirectoryCommand.__validMagentoPaths(path)
         if result is None:
             return None
 
         from_path_rel = result.get('from')
         to_path_rel = result.get('to')
 
-        result = MagentoUtilsCopyToPackageCommand.__getBasePathAndRelPath(path, from_path_rel)
+        result = MagentoUtilsCopyToOtherDirectoryCommand.__getBasePathAndRelPath(path, from_path_rel)
         if result is None:
             return None
 
         base_path = result.get('base')
         path_rel = result.get('rel_path')
 
-        return MagentoUtilsCopyToPackageCommand.__getToPath(base_path, to_path_rel, path_rel)
+        return MagentoUtilsCopyToOtherDirectoryCommand.__getToPath(base_path, to_path_rel, path_rel)
 
     @staticmethod
     def __getToPath(base_path, to_path_rel, path_rel):
         to_path = os.path.join(base_path, to_path_rel, path_rel)
-        if '!CUSTOM_PACKAGE!' in to_path:
-            custom_package = MagentoUtilsCopyToPackageCommand.__figureOutCustomPackage(base_path)
-            to_path = to_path.replace('!CUSTOM_PACKAGE!', custom_package)
+        if '!GUESS_CUSTOM_PACKAGE!' in to_path:
+            custom_package = MagentoUtilsCopyToOtherDirectoryCommand.__guessCustomPackage(base_path)
+            to_path = to_path.replace('!GUESS_CUSTOM_PACKAGE!', custom_package)
 
         return to_path
 
     @staticmethod
     def __validMagentoPaths(path):
-        for from_path, to_path in MagentoUtilsCopyToPackageCommand.copy_paths.items():
+        for from_path, to_path in MagentoUtilsCopyToOtherDirectoryCommand.__getCopyPaths().items():
             if from_path in path:
                 return {'from': from_path, 'to': to_path}
 
         return None
 
     @staticmethod
-    def __figureOutCustomPackage(base_path):
+    def __guessCustomPackage(base_path):
         base_path = os.path.join(base_path, 'app/design/frontend')
         if os.path.isdir(base_path):
             for f in os.listdir(base_path):
                 if os.path.isdir(os.path.join(base_path, f)):
-                    if f != 'base' and f != 'default':
+                    if f != 'base' and f != 'default' and f != 'enterprise':
                         return f
 
         return None
--- README.md
+++ README.md
@@ -1,6 +1,52 @@
 # Magento Utils plugin for Sublime Text 2
 
-Currently only provides one functionality:
+## Provides copying specific files from one directory to another
 
-* When right clicking on a file or directory inside the base or default packages from magento, it will show a menu item to copy that file or directory into your custom package.
-* Also does the same for code/core and code/community => code/local directories
+When right clicking on a file or directory in the sidebar, you will have the option to copy it from one directory to another.
+The plugin will first look if you already have a copy at the destination and if it finds one, it won't allow you to copy it anymore as it already exists.
+
+The following directories are considered:
+
+* Files from `app/code/core/...` or `app/code/community/...` to `app/code/local/...`
+* Files from `app/design/frontend/{package}/{theme}` to another package/theme combination
+* Files from `skin/frontend/{package}/{theme}` to another package/theme combination
+
+You can define the package/theme copying on a per-project base by defining this setting in your .sublime-project file:
+
+```
+"settings":
+{
+    "magento_utils_packages_to_copy_between":
+    {
+        "base/default/": "custom/default/",
+        "default/default/": "custom/default/",
+        "parent/default": "custom/default"     # Usefull for Magento CE >= 1.9 parent/child theme support
+    }
+}
+```
+If there isn't a project setting, the plugin will try to guess your custom package, by looking at the available packages and taking the first one which isn't 'base', 'default' or 'enterprise'.
+
+
+
+## Roadmap
+
+I'm considering of adding a feature where you can copy a template from the same source to multiple destinations, and giving you the option to choose which one out of those destinations you want to use, this will allow you to do something like this:
+
+```
+"settings":
+{
+    "magento_utils_packages_to_copy_between":
+    {
+        "base/default/": "custom/default/",
+        "default/default/": "custom/default/",
+        "base/default/": "custom/christmas/",
+        "default/default/": "custom/christmas/",
+        "custom/default/": "custom/christmas/"
+    }
+}
+```
+
+## Issues
+
+I noticed a certain issue when sometimes the menu item isn't showing when it should, you can get around by right clicking the file a second time, then it will appear again.  
+(For future notice: it has something to do with the fact that the plugin can't get access to the project settings for some strange reason...)
